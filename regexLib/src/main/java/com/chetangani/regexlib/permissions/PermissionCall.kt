ac44af979208ad2db039a16b997b6dfa588d29a5
ChetanG <cgani87@gmail.com>
2022-03-27 11:54:14 +0530

    Updated Permission Call Storage permission check on 27th March 2022

--- regexLib/src/main/java/com/chetangani/regexlib/permissions/PermissionCall.kt
+++ regexLib/src/main/java/com/chetangani/regexlib/permissions/PermissionCall.kt
@@ -1,7 +1,9 @@
 package com.chetangani.regexlib.permissions
 
+import android.Manifest
 import android.annotation.TargetApi
 import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -12,9 +14,11 @@ import com.chetangani.regexlib.constants.ConstantValues.TRUE
 import com.chetangani.regexlib.interfaces.PermissionCallInterface
 import com.chetangani.regexlib.interfaces.PermissionReceiver
 
-class PermissionCall(private val activity: AppCompatActivity, private val permissions: Array<String>,
-                     private val requestPermissionCode: Int,
+class PermissionCall(private val activity: AppCompatActivity,
                      private val permissionReceiver: PermissionReceiver): PermissionCallInterface {
+
+    private var requestPermissionCode: Int = 0
+    private lateinit var permissions: Array<String>
 
     companion object {
         lateinit var permissionCallInterface: PermissionCallInterface
@@ -22,22 +26,30 @@ class PermissionCall(private val activity: AppCompatActivity, private val permis
 
     init {
         permissionCallInterface = this
-        checkPermissionsMAndAbove()
     }
 
-    @TargetApi(23)
-    private fun checkPermissionsMAndAbove() {
+    @TargetApi(Build.VERSION_CODES.M)
+    private fun checkPermissionsMAndAbove(requestedPermission: Array<String>) {
+        requestPermissionCode = OTHER_PERMISSION
+        permissions = requestedPermission
         if (checkPermission()) permissionReceiver.permissionGranted(TRUE) else requestPermission()
     }
 
-    @TargetApi(23)
+    @TargetApi(Build.VERSION_CODES.M)
+    fun checkStoragePermission() {
+        requestPermissionCode = STORAGE_PERMISSION
+        permissions = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        if (checkPermission()) permissionReceiver.storagePermissionGranted(TRUE) else requestPermission()
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
     private fun requestPermission() {
         ActivityCompat.requestPermissions(
             activity, permissions, requestPermissionCode
         )
     }
 
-    @TargetApi(23)
+    @TargetApi(Build.VERSION_CODES.M)
     private fun checkPermission(): Boolean {
         val resultList: MutableList<Boolean> = ArrayList()
         for (i in permissions.indices)
@@ -45,19 +57,21 @@ class PermissionCall(private val activity: AppCompatActivity, private val permis
         return !resultList.contains(FALSE)
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
-                                            grantResults: IntArray, requestedPermission: Int) {
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         if ((requestCode == OTHER_PERMISSION) || (requestCode == STORAGE_PERMISSION)) {
             if (grantResults.isNotEmpty()) {
                 val permissionList: MutableList<Boolean> = ArrayList()
                 for (i in grantResults.indices)
                     permissionList.add(grantResults[i] == PackageManager.PERMISSION_GRANTED)
                 if (!permissionList.contains(FALSE)) {
-                    if (requestedPermission == OTHER_PERMISSION)
+                    if (requestPermissionCode == OTHER_PERMISSION)
                         permissionReceiver.permissionGranted(TRUE)
-                    else if (requestedPermission == STORAGE_PERMISSION)
+                    else if (requestPermissionCode == STORAGE_PERMISSION)
                         permissionReceiver.storagePermissionGranted(TRUE)
-                } else checkPermissionsMAndAbove()
+                } else {
+                    if (requestPermissionCode == OTHER_PERMISSION) checkPermissionsMAndAbove(permissions)
+                    else if (requestPermissionCode == STORAGE_PERMISSION) checkStoragePermission()
+                }
             }
         }
     }
